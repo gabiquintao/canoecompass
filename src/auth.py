42973dae5deecd9d365ad75aa22a73d6eea61bb9
gabiquintao <gabriel.quintao@icloud.com>
2026-08-10 13:28:27 +0100

    fix: resolve race condition when saving new users

--- src/auth.py
+++ src/auth.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from fastapi import HTTPException, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from sqlalchemy.exc import IntegrityError
 from supabase import create_client
 
 from database import SessionLocal, User
@@ -32,9 +33,12 @@ def get_current_user_id(
         with SessionLocal() as db:
             user = db.query(User).filter(User.id == user_id).first()
             if not user:
-                user = User(id=user_id)
-                db.add(user)
-                db.commit()
+                try:
+                    user = User(id=user_id)
+                    db.add(user)
+                    db.commit()
+                except IntegrityError:
+                    db.rollback()
 
         return user_id
     except Exception as e:
